a5a6c1ed10ccbb9accee4ada1402b3141c596cf2
Drachir000 <contact@drachir000.de>
2026-06-26 13:04:55 +0200

    Add recipes for hammers
    Updated `ModRecipeProvider` to handle data generation for hammer recipes. Added generated json files for recipes.
    
    Signed-off-by: Drachir000 <contact@drachir000.de>

--- src/main/java/de/drachir000/simplehammers/datagen/ModRecipeProvider.java
+++ src/main/java/de/drachir000/simplehammers/datagen/ModRecipeProvider.java
@@ -1,9 +1,16 @@
 package de.drachir000.simplehammers.datagen;
 
+import de.drachir000.simplehammers.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -15,8 +22,27 @@ public class ModRecipeProvider extends RecipeProvider {
 	
 	@Override
 	protected void buildRecipes() {
+		
+		hammer(ModItems.WOODEN_HAMMER.get(), ItemTags.WOODEN_TOOL_MATERIALS, Items.STICK);
+		hammer(ModItems.COPPER_HAMMER.get(), ItemTags.COPPER_TOOL_MATERIALS, Items.COPPER_INGOT);
+		hammer(ModItems.STONE_HAMMER.get(), ItemTags.STONE_TOOL_MATERIALS, Items.COBBLESTONE);
+		hammer(ModItems.GOLDEN_HAMMER.get(), ItemTags.GOLD_TOOL_MATERIALS, Items.GOLD_INGOT);
+		hammer(ModItems.IRON_HAMMER.get(), ItemTags.IRON_TOOL_MATERIALS, Items.IRON_INGOT);
+		hammer(ModItems.DIAMOND_HAMMER.get(), ItemTags.DIAMOND_TOOL_MATERIALS, Items.DIAMOND);
+		
+		this.netheriteSmithing(ModItems.DIAMOND_HAMMER.get(), RecipeCategory.TOOLS, ModItems.NETHERITE_HAMMER.get());
 	
+	}
 	
+	private void hammer(Item result, TagKey<Item> ingredientTag, ItemLike unlockItem) {
+		shaped(RecipeCategory.TOOLS, result)
+				.pattern("XXX")
+				.pattern("X#X")
+				.pattern(" # ")
+				.define('#', Items.STICK)
+				.define('X', ingredientTag)
+				.unlockedBy(getHasName(unlockItem), has(ingredientTag))
+				.save(output);
 	}
 	
 	public static class Runner extends RecipeProvider.Runner {
